eed37c2ee57a6993694ec05db183196dcde60426
Abdul Sahil <sahiilwork444@gmail.com>
2026-03-30 19:17:25 +0530

    new commit

--- NLPApp/myapi.py
+++ NLPApp/myapi.py
@@ -5,7 +5,7 @@ import nlpcloud
 class API:
 
     def __init__(self):
-        self.client = nlpcloud.Client("gpt-oss-120b", "c79e25f38ae7faadb017626aabc0ec7f598a5448", gpu=True)
+        self.client = nlpcloud.Client("gpt-oss-120b", "09d6a6394e07c15b984dc4b90ef0de12834d88ca", gpu=True)
         
 
     def sentiment_analysis(self,para):
